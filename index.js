4025da02eb81d81001ea725bf4cfa7a3d41f62e8
james <jaydhales@gmail.com>
2021-10-13 15:36:36 +0100

    added hamburger menu

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+let toggler = document.querySelector('.nav-toggle');
+const navBar = document.querySelector('.nav-links');
+
 function reseter () {
     document.querySelector("form").submit();
     document.querySelector("form").reset();
 }
 
+toggler.addEventListener('click', (e) => {
+    navBar.classList.toggle('show-nav');
+    toggler.classList.toggle('rotate');
+    const ham = toggler.firstElementChild.classList;
+
+    setInterval(() => {
+        if (ham.contains("fa-bars")) {
+            ham.replace('fa-bars','fa-times')
+        } else {
+            ham.replace('fa-times','fa-bars')
+        }
+    }, 300);
+})
