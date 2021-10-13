ff88126e269218e8286b592631b5e48fd446544a
james <jaydhales@gmail.com>
2021-10-13 17:48:07 +0100

    update navbar

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let toggler = document.querySelector('.nav-toggle');
+const toggler = document.querySelector('.nav-toggle');
 const navBar = document.querySelector('.nav-links');
 
 function reseter () {
@@ -9,6 +9,7 @@ function reseter () {
 toggler.addEventListener('click', (e) => {
     navBar.classList.toggle('show-nav');
     toggler.classList.toggle('rotate');
+
     const ham = toggler.firstElementChild.classList;
 
     setInterval(() => {
@@ -18,4 +19,12 @@ toggler.addEventListener('click', (e) => {
             ham.replace('fa-times','fa-bars')
         }
     }, 300);
+})
+
+window.addEventListener('scroll' , (e) => {
+    if (navBar.getBoundingClientRect().y < -120) {
+        toggler.firstElementChild.classList.replace('fa-times','fa-bars');
+        navBar.classList.remove('show-nav');
+        toggler.classList.remove('rotate');
+    }
 })
